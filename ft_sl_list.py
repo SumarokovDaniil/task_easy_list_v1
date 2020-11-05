33973fe9fa27eaf64de271ae919dd491a5ecdc57
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 10:34:54 +0300

    Update ft_sl_list.py

--- ft_sl_list.py
+++ ft_sl_list.py
@@ -1,6 +1,13 @@
+def ft_len(mass):
+    count = 0
+    for _ in mass:
+        count += 1
+    return count
+
+
 def ft_sl_list(mass):
     count = 0
-    for i in range(1, len(mass)):
+    for i in range(1, ft_len(mass)):
         if mass[i] > mass[i - 1]:
             count += 1
     return count
