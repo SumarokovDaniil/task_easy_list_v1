24947924febb4d79b5639eec8cf85d84c464cce2
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 10:34:13 +0300

    Update ft_rshift_list.py

--- ft_rshift_list.py
+++ ft_rshift_list.py
@@ -1,6 +1,13 @@
+def ft_len(mass):
+    count = 0
+    for _ in mass:
+        count += 1
+    return count
+
+
 def ft_rshift_list(mass):
     first = mass[-1]
-    for i in range(len(mass) - 1, 0, -1):
+    for i in range(ft_len(mass) - 1, 0, -1):
         mass[i] = mass[i - 1]
     mass[0] = first
     return mass
