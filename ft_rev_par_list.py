fc68bf2851103f73d4b565256e83d898cb3e7c78
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 09:36:18 +0300

    Update ft_rev_par_list.py

--- ft_rev_par_list.py
+++ ft_rev_par_list.py
@@ -10,3 +10,9 @@ def ft_rev_par_list(mass):
         new_mass.append(mass[i - 1])
     new_mass.append(mass[-1])
     return new_mass
+
+
+def ft_rev_list(mass):
+    for i in range(len(mass)):
+        mass[i] = len(mass) - i
+    return mass
