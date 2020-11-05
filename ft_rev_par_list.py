5a841f310b0f1deb318aa51a1db018a204169144
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 09:44:18 +0300

    Update ft_rev_par_list.py

--- ft_rev_par_list.py
+++ ft_rev_par_list.py
@@ -1,16 +1,19 @@
 def ft_rev_list(mass):
-    for i in range(len(mass)):
-        mass[i] = len(mass) - i
-    return mass
+    new_mass = []
+    for i in range(-1, -len(mass) - 1, -1):
+        new_mass.append(mass[i])
+    return new_mass
 
 
 def ft_rev_par_list(mass):
     new_mass = list()
+
     if len(mass) % 2 == 0:
         for i in range(1, len(mass), 2):
             new_mass.append(mass[i])
             new_mass.append(mass[i - 1])
-        return new_mass
+        return ft_rev_list(new_mass)
+
     for i in range(1, len(mass), 2):
         new_mass.append(mass[i])
         new_mass.append(mass[i - 1])
