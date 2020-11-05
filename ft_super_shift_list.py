32b6e2f4d1306cb8850944c0fa0ef000c686fe64
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 10:39:31 +0300

    Update ft_super_shift_list.py

--- ft_super_shift_list.py
+++ ft_super_shift_list.py
@@ -1,9 +1,24 @@
-def ft_slice_str(mass, start, stop):
-    result_mass = []
-    for i in range(start, stop):
-        result_mass.append(mass[i])
-    return result_mass
+def ft_len(mass):
+    count = 0
+    for _ in mass:
+        count += 1
+    return count
+
+
+def ft_rshift_list(mass):
+    first = mass[-1]
+    for i in range(len(mass) - 1, 0, -1):
+        mass[i] = mass[i - 1]
+    mass[0] = first
+    return mass
 
 
 def ft_super_shift_list(mass, n):
-    return ft_slice_str(mass, -n, 0) + ft_slice_str(mass, 0, -n)
+    if n > 0:
+        for _ in range(n):
+            mass = ft_rshift_list(mass)
+        return mass
+    else:
+        for _ in range(ft_len(mass) + n):
+            mass = ft_rshift_list(mass)
+        return mass
