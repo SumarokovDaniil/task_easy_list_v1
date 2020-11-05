0ac63d03eb59cdbea0c27f2c0cf423a9905eeb53
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 09:30:53 +0300

    Update ft_even_index_list.py

--- ft_even_index_list.py
+++ ft_even_index_list.py
@@ -1,6 +1,6 @@
 def ft_even_index_list(mass):
     new_mass = list()
     for i in range(len(mass)):
-        if i % 2 == 1:
+        if i % 2 == 0:
             new_mass.append(mass[i])
     return new_mass
