fda6df4b7b766d4a2d89148dd1c9a0603848696f
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 10:32:32 +0300

    Update ft_even_index_list.py

--- ft_even_index_list.py
+++ ft_even_index_list.py
@@ -1,6 +1,13 @@
+def ft_len(mass):
+    count = 0
+    for _ in mass:
+        count += 1
+    return count
+
+
 def ft_even_index_list(mass):
     new_mass = list()
-    for i in range(len(mass)):
+    for i in range(ft_len(mass)):
         if i % 2 == 0:
             new_mass.append(mass[i])
     return new_mass
