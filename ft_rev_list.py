b52ed102b0b3f7027f0b1b6c6121ad272fd9d26a
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 10:33:22 +0300

    Update ft_rev_list.py

--- ft_rev_list.py
+++ ft_rev_list.py
@@ -1,4 +1,11 @@
+def ft_len(mass):
+    count = 0
+    for _ in mass:
+        count += 1
+    return count
+
+
 def ft_rev_list(mass):
-    for i in range(len(mass)):
-        mass[i] = len(mass) - i
+    for i in range(ft_len(mass)):
+        mass[i] = ft_len(mass) - i
     return mass
