76ace35bb199f8ba9b55086f53680ae088a9ac2e
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-11-05 10:34:33 +0300

    Update ft_same_parts_list.py

--- ft_same_parts_list.py
+++ ft_same_parts_list.py
@@ -1,5 +1,12 @@
+def ft_len(mass):
+    count = 0
+    for _ in mass:
+        count += 1
+    return count
+
+
 def ft_same_parts_list(mass):
-    for i in range(1, len(mass)):
+    for i in range(1, ft_len(mass)):
         if mass[i] > 0 and mass[i - 1] > 0 or \
                 mass[i] < 0 and mass[i - 1] < 0:
             return True
